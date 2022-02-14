b0de6f8003ca977f699b5229f849e5579d65b297
TheLuisAbarca <arclaav.adm@gmail.com>
2022-02-14 15:37:47 -0500

    Implement all the statements concerned to this task.

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id              INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name            VARCHAR(255) NOT NULL,
+    date_of_birth   DATE NOT NULL,
+    escape_attempts INT DEFAULT 0,
+    neutered        BOOLEAN DEFAULT FALSE,
+    weight_kg       DECIMAL(5,2) NOT NULL
 );
